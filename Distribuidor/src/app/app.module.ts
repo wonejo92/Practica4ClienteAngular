64c02ffaa3e5974ae16e03f5e7bc1972d05ab0f3
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2020-07-11 15:47:57 -0500

    Creacion del componente Producto.

--- Distribuidor/src/app/app.module.ts
+++ Distribuidor/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './Components/productos/productos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
