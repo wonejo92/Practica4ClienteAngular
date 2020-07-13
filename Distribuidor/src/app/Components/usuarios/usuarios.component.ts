a640322281bc2e829aec420061006bbe3844cb7a
HelenCVM <52894597+HelenCVM@users.noreply.github.com>
2020-07-12 21:06:57 -0500

    Parametros get

--- Distribuidor/src/app/Components/usuarios/usuarios.component.ts
+++ Distribuidor/src/app/Components/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormGroup} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -18,12 +19,12 @@ export class UsuariosComponent implements OnInit {
   contrasena : new FormControl('')
   });
   
-  constructor() { }
-
+  constructor(protected http: HttpClient) { }
+ 
   ngOnInit(): void {
   }
   
   guardarDatos() {
-   
+    return this.http.get('https://localhost:8080/');
   }
 }
