9ed472703c762a99f7e64556d0ee2cf199bb913f
HelenCVM <52894597+HelenCVM@users.noreply.github.com>
2020-07-21 16:52:03 -0500

    parametros entre paginas

--- Distribuidor/src/app/Components/modificar/modificar.component.ts
+++ Distribuidor/src/app/Components/modificar/modificar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit ,Input} from '@angular/core';
 import { IniciosesionService } from '../../Services/iniciosesion.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Usuario } from '../../Modelo/persona';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modificar',
@@ -12,7 +12,7 @@ import { Usuario } from '../../Modelo/persona';
 export class ModificarComponent implements OnInit {
   public correop:any
   usuario:Usuario
-
+  user:FormGroup
   @Input() ActualizarDetails = {
     nombres:'',apellidos:'',telefono:'',correo:'', contrasena:''
   }
@@ -22,6 +22,14 @@ export class ModificarComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.user = new FormGroup({
+      nombres: new FormControl('', Validators.required),
+      apellidos:new FormControl('', Validators.required),
+      telefono:new FormControl('', Validators.required),
+      correo:new FormControl('', Validators.required),
+      contrasena:new FormControl('', Validators.required)
+
+  });
   }
 
 
@@ -31,4 +39,9 @@ export class ModificarComponent implements OnInit {
     this.router.navigate(['/InicioSesion'])
   })
 }
+
+atras(){
+  this.router.navigate(['/home/',this.correop])
+}
+
 }
